0c094c3d7b36d82cc8e420db68dae0deb4d5a570
Stephen Hung <hung.stephen.yh@gmail.com>
2017-06-17 17:50:29 -0700

    Learned about variable initialization + declaration

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Variables
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Declare Variables.
+            int x;
+            int y;
+
+            // Initialize Variables.
+            x = 7;
+            y = x + 3;
+
+            // Print it out to console and use a readline to stop console from crashing.
+            Console.WriteLine(y);
+            Console.ReadLine();
+
+            /*
+             * Declares block comments
+             * 
+             */
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+            // Gets input from console and assigns it to String Variable myFirstName
+
+            Console.Write("Type your last name: ");
+            string myLastname = Console.ReadLine();
+            // Gets input from console and assigns it to String Variable myLastName
+
+            Console.WriteLine("Hello " + myFirstName + " " + myLastName);
+        }
+    }
+}
