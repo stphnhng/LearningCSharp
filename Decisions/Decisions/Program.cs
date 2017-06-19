8e64a616c2af6f1eef8c0736d797ad0402acdcfb
Stephen Hung <hung.stephen.yh@gmail.com>
2017-06-18 19:25:31 -0700

    Added string format

--- Decisions/Decisions/Program.cs
+++ Decisions/Decisions/Program.cs
@@ -6,23 +6,47 @@ namespace Decisions
     {
         public static void Main(string[] args)
         {
+            // Regular If/Else statements
+            /*
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1, 2 or 3: ");
+            string userValue = Console.ReadLine();
+            string message;
+
+            if(userValue == "1")
+                // First decision block
+                message = "You won a new car!";
+            else if(userValue == "2")
+                // checks if the first decision block is false
+                message = "You won a new boat!";
+            else if(userValue == "3")
+                message = "You won a new cat!";
+            else
+                // Checks if all decisions blocks are false
+                message = "Sorry, we didn't understand that";
+            Console.WriteLine(message);
+            Console.ReadLine();
+            */
+
+            // Shortened If/Else statements
+            // Doesn't work for multiple conditions.
             Console.WriteLine("Bob's Big Giveaway");
             Console.Write("Choose a door: 1, 2 or 3: ");
             string userValue = Console.ReadLine();
 
-            if(userValue == "1"){
-                string message = "You won a new car!";
-                Console.WriteLine(message);
-            }
-            else if(userValue == "2"){
-                string message = "You won a new boat!";
-                Console.WriteLine(message);
-            }
-            else if(userValue == "3"){
-                string message = "You won a new cat!";
-                Console.WriteLine(message);
-            }
+            string message = (userValue == "1") ? "boat" : "strand of hair";
+
+            //Console.Write("You won a ");
+            //Console.Write(message);
+            //Console.Write(".");
+            // ^^ can be written as such....
+
+            Console.WriteLine("You entered: {0}, therefore you won a {1}.", userValue, message);
+            // Similar to String.format.
+
             Console.ReadLine();
+
+
         }
     }
 }
