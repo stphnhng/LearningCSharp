017a5af440f614049a7c40f3e94b0fa7c16fe4a5
Stephen Hung <hung.stephen.yh@gmail.com>
2017-06-19 21:07:01 -0700

    Using for loops in C#

---
+++ ForIteration/ForIteration/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ForIteration
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                // For loop from 0 to 9.
+                //Console.WriteLine(i);
+                if(i == 7){
+                    // if i == 7 then print out it is found.
+                    Console.WriteLine("Found seven!");
+                    break;
+                }
+            }
+
+            for (int i = 0; i < 12; i++)
+            {
+                
+            }
+            // Visual studio has code snippet for for loop (press tab tab)
+
+
+            Console.ReadLine();
+
+            // Debugger = F9
+            // Step onto (next step) = F10
+            // Can use it for conditional breaks (breaks when something is true)
+
+        }
+    }
+}
