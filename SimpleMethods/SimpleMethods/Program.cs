562c46750f3f2ec2b2bf90c374bc1e93fa230ba5
Stephen Hung <hung.stephen.yh@gmail.com>
2017-06-20 16:24:11 -0700

    Learning how to use the most basic method

---
+++ SimpleMethods/SimpleMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SimpleMethods
+{
+    class MainClass
+    {
+        public static void Main(string[] args){
+            HelloWorld();
+        }
+
+        private static void HelloWorld(){
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This was printed in a method and not a main method");
+        }
+
+    }
+}
