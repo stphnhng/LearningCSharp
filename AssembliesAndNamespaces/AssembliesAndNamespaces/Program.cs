cf85b38ba91a0c6357d86836ba3063be1659db39
Stephen Hung <hung.stephen.yh@gmail.com>
2017-06-24 20:39:07 -0700

    Learning about namespaces and using microsoft resources

---
+++ AssembliesAndNamespaces/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace AssembliesAndNamespaces
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+            // site:microsoft.com question - looks up microsoft code to do something.
+
+            Console.WriteLine(reply);
+
+            string text = "stuff inputted to a file";
+
+            //System.IO.File.WriteAllText("/Users/stephen/desktop/C#/learning/Assembliesandnamespaces", text);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
