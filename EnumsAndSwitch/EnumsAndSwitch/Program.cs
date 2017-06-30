cd052d65f38b1f28f28e727557d2c249c404d74e
Stephen Hung <hung.stephen.yh@gmail.com>
2017-06-30 19:05:51 +0800

    Learning how to use enums and switch statements

---
+++ EnumsAndSwitch/EnumsAndSwitch/Program.cs
@@ -0,0 +1,82 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsAndSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // enum limits and constrains all possible values to ensure that it 
+            // is valid in the system and has meaning.
+            // limit the number of options a status property of a to-do list.
+
+            List<Todo> todos = new List<Todo>()
+            {
+                new Todo { Description = "Task 1", EstimatedHours = 6, Status = Status.Completed },
+                new Todo { Description = "Task 2", EstimatedHours = 2, Status = Status.InProgress },
+                new Todo { Description = "Task 3", EstimatedHours = 8, Status = Status.NotStarted },
+                new Todo { Description = "Task 4", EstimatedHours = 12, Status = Status.Deleted },
+                new Todo { Description = "Task 5", EstimatedHours = 6, Status = Status.InProgress },
+                new Todo { Description = "Task 6", EstimatedHours = 2, Status = Status.NotStarted },
+                new Todo { Description = "Task 7", EstimatedHours = 14, Status = Status.NotStarted },
+                new Todo { Description = "Task 8", EstimatedHours = 8, Status = Status.Completed },
+                new Todo { Description = "Task 9", EstimatedHours = 8, Status = Status.InProgress },
+                new Todo { Description = "Task 10", EstimatedHours = 8, Status = Status.Completed },
+                new Todo { Description = "Task 11", EstimatedHours = 4, Status = Status.NotStarted },
+                new Todo { Description = "Task 12", EstimatedHours = 10, Status = Status.Completed },
+                new Todo { Description = "Task 13", EstimatedHours = 12, Status = Status.Deleted },
+                new Todo { Description = "Task 14", EstimatedHours = 6, Status = Status.Completed }
+            };
+
+            PrintAssessment(todos);
+        }
+
+        private static void PrintAssessment(List<Todo> todos)
+        {
+            foreach (var todo in todos)
+            {
+                switch (todo.Status)
+                {
+                    case Status.NotStarted:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                    case Status.InProgress:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    case Status.OnHold:
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        break;
+                    case Status.Completed:
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        break;
+                    case Status.Deleted:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine(todo.Description);
+            }
+        }
+    }
+
+    class Todo
+    {
+        public string Description { get; set; }
+        public int EstimatedHours { get; set; }
+        public Status Status { get; set; }
+    }
+
+    enum Status
+    {
+        NotStarted,
+        InProgress,
+        OnHold,
+        Completed,
+        Deleted
+    }
+}
