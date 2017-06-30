635d9cb633b86c63095ba00457a7c1b9cb0ffde5
Stephen Hung <hung.stephen.yh@gmail.com>
2017-06-30 09:46:28 +0800

    Combining assemblies and client into a single solution

---
+++ Lesson18/ScrapeClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ScrapeLibrary;
+
+namespace ScrapeClient
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Scrape myScrape = new Scrape();
+            string value = myScrape.ScrapeWebpage("http://msdn.microsoft.com");
+            Console.WriteLine(value);
+        }
+    }
+}
