5fe2b21015525706c46d7629183d45a2a5cebcfd
Stephen Hung <hung.stephen.yh@gmail.com>
2017-06-30 09:45:59 +0800

    Learning to create a client that uses 3rd party assemblies and how to create a class library

---
+++ MyClient/MyClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyCodeLibrary; // uses MyCodeLibrary to scrape webpage
+
+namespace MyClient
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Scrape myScrape = new Scrape();
+            string value = myScrape.ScrapeWebpage("http://msdn.microsoft.com");
+            Console.WriteLine(value);
+        }
+    }
+}
